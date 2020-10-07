25a71fe8529ea50153b0c09d057d6040dc2bdf78
BluishGreenPro <bluish-green_productions@hotmail.com>
2020-10-06 21:11:45 -0400

    Tweaked MenuNodeSlider

--- Assets/UI/UniNav System/MenuNodeSlider.cs
+++ Assets/UI/UniNav System/MenuNodeSlider.cs
@@ -10,6 +10,15 @@ public class MenuNodeSlider : MenuNode
 
     private float valueOriginal = 0f;
 
+    private void Awake() {
+        if (mAccept != null) {
+    //Inherit directions
+            mUp = mAccept.mUp;
+            mDown = mAccept.mDown;
+            mForward = mAccept.mForward;
+            mBackward = mAccept.mBackward;
+        }
+    }
     /*
     private void OnEnable() {
         navButton.OnSelect += NavButton_OnSelect;
@@ -35,13 +44,10 @@ public class MenuNodeSlider : MenuNode
     }
 
     public override void MenuNavigate(NavDir navDir) {
-        Debug.Log("navDir: "+navDir);
-        //MenuNode _mNode = null;
+        //Debug.Log("navDir: "+navDir);
+        MenuNode _mNode = null;
         switch (navDir) {
-            case NavDir.Accept:
-                Debug.Log("mAccept: "+mAccept);
-                MenuNavigator.Instance.MenuFocus(mAccept);
-                break;
+            case NavDir.Accept: _mNode = mAccept; break;
             case NavDir.Cancel:
         //Reset the value
                 slider.value = valueOriginal;
@@ -49,20 +55,16 @@ public class MenuNodeSlider : MenuNode
                 break;
             case NavDir.Left: slider.value -= sliderDelta; break;
             case NavDir.Right: slider.value += sliderDelta; break;
-            /*
             case NavDir.Up: _mNode = mUp; break;
             case NavDir.Down: _mNode = mDown; break;
             case NavDir.Forward: _mNode = mForward; break;
             case NavDir.Backward: _mNode = mBackward; break;
-            //*/
         }
         //Debug.Log("_mNode: "+_mNode);
-        /*
         if (_mNode != null && _mNode.validSelection)
             MenuNavigator.Instance.MenuFocus(_mNode);
         else {
             Debug.Log("_mNode is null");
         }
-        //*/
     }
 }
