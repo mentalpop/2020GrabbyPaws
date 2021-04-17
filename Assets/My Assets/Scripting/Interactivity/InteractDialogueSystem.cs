3af28a10ee09f5aa612263b88e349b7181a51e92
BluishGreenPro <bluish-green_productions@hotmail.com>
2021-04-17 16:42:01 -0400

    Added InteractDialogueSystem

---
+++ Assets/My Assets/Scripting/Interactivity/InteractDialogueSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class InteractDialogueSystem : Interactable
+{
+    public DialogueSystemTrigger DSTrigger;
+    public override void Interact() {
+        //base.Interact();
+        DSTrigger.OnUse();
+    }
+}
