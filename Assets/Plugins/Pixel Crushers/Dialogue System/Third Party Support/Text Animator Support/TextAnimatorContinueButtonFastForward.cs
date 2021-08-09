2c9e994e6610fac554a7b3b664623f0821e43c49
Kaitlyn <43460243+mentalpop@users.noreply.github.com>
2021-08-09 10:34:54 -0400

    Text Test NPC added

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Third Party Support/Text Animator Support/TextAnimatorContinueButtonFastForward.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Pixel Crushers. All rights reserved.
+
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    /// <summary>
+    /// This script replaces the normal continue button functionality with
+    /// a two-stage process. If Text Animator's typewriter is still playing, it
+    /// simply fast forwards to the end. Otherwise it sends OnContinue to the UI.
+    /// </summary>
+    public class TextAnimatorContinueButtonFastForward : MonoBehaviour
+    {
+
+        [Tooltip("Dialogue UI that the continue button affects.")]
+        public StandardDialogueUI dialogueUI;
+
+        [Tooltip("Text Animator Player to fast forward if it's not done playing.")]
+        public Febucci.UI.TextAnimatorPlayer textAnimatorPlayer;
+
+        [Tooltip("Hide the continue button when continuing.")]
+        public bool hideContinueButtonOnContinue = false;
+
+        private UnityEngine.UI.Button continueButton;
+
+        private AbstractDialogueUI m_runtimeDialogueUI;
+        private AbstractDialogueUI runtimeDialogueUI
+        {
+            get
+            {
+                if (m_runtimeDialogueUI == null)
+                {
+                    m_runtimeDialogueUI = dialogueUI;
+                    if (m_runtimeDialogueUI == null)
+                    {
+                        m_runtimeDialogueUI = GetComponentInParent<AbstractDialogueUI>();
+                        if (m_runtimeDialogueUI == null)
+                        {
+                            m_runtimeDialogueUI = DialogueManager.dialogueUI as AbstractDialogueUI;
+                        }
+                    }
+                }
+                return m_runtimeDialogueUI;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            continueButton = GetComponent<UnityEngine.UI.Button>();
+        }
+
+        public virtual void OnFastForward()
+        {
+            if (textAnimatorPlayer != null && !textAnimatorPlayer.textAnimator.allLettersShown)
+            {
+                textAnimatorPlayer.SkipTypewriter();
+            }
+            else
+            {
+                if (hideContinueButtonOnContinue && continueButton != null) continueButton.gameObject.SetActive(false);
+                if (runtimeDialogueUI != null) runtimeDialogueUI.OnContinue();
+            }
+        }
+
+    }
+}
---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Third Party Support/Text Animator Support/_README.txt
@@ -0,0 +1,6 @@
+Basic Dialogue System integration steps are on Febucci's site:
+
+https://www.notion.so/Dialogue-Systems-Plugins-Integrations-298ffd1f69484507a0532c2b0fdd13b8#5c4379bb693e410c8ce70880cbb49ea3
+
+This folder contains a custom continue button fast forward script
+that works with a Text Animator subtitle text.
