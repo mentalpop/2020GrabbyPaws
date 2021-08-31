43d514ae32bf64733fef33c45a56d9d385acc32f
Kaitlyn <43460243+mentalpop@users.noreply.github.com>
2021-08-31 19:32:43 -0400

    Emitter Test

---
+++ Assets/My Assets/Scripting/Kaitlyn's not a programmer but she tries/SnackEmit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Place this on a particle system with a trigger module enabled
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class SnackEmit : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    ParticleSystem Ps;
+    List<ParticleSystem.Particle> Enter;
+    void Awake()
+    {
+        Ps = GetComponent<ParticleSystem>();
+        Enter = new List<ParticleSystem.Particle>();
+    }
+
+    void OnValidate()
+    {
+        Ps = GetComponent<ParticleSystem>();
+        var trigger = Ps.trigger;
+        if (trigger.enter != ParticleSystemOverlapAction.Callback)
+        {
+            trigger.enter = ParticleSystemOverlapAction.Callback;
+        }
+    }
+
+    void OnParticleTrigger()
+    {
+        // get
+        int numInside = Ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, Enter);
+
+        // on enter
+        for (int i = 0; i < numInside; i++)
+        {
+            ParticleSystem.Particle particle = Enter[i];
+
+            Instantiate(Prefab, particle.position, Quaternion.identity);
+        }
+    }
+}
