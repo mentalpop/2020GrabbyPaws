81d55357fefe06d573ba7693b86c46549740ff7e
BluishGreenPro <bluish-green_productions@hotmail.com>
2021-11-04 17:44:50 -0400

    Simple Spotlight which follows Mouse

---
+++ Assets/UI/Title Screen/SpotLightHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLightHandler : MonoBehaviour
+{
+    public RectTransform spotlight;
+    public Canvas myCanvas;
+
+    private void Update() {
+        //spotlight.localPosition = Input.mousePosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out Vector2 pos);
+        spotlight.position = myCanvas.transform.TransformPoint(pos);
+    }
+}
