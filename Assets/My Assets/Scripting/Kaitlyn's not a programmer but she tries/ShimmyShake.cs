c9b0477fe051d74a9c8794204e8c7ad21636c863
Kaitlyn <43460243+mentalpop@users.noreply.github.com>
2021-12-13 10:33:16 -0500

    IceBlocks
    Gifts, New Pumps, Soup and Shake Script

---
+++ Assets/My Assets/Scripting/Kaitlyn's not a programmer but she tries/ShimmyShake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShimmyShake : MonoBehaviour
+{
+    public float amplitudeX = 10.0f;
+    public float amplitudeY = 5.0f;
+    public float omegaX = 1.0f;
+    public float omegaY = 5.0f;
+    float index;
+
+    public void Update()
+    {
+        index += Time.deltaTime;
+        float x = amplitudeX * Mathf.Cos(omegaX * index);
+        float y = Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
+        transform.localPosition = new Vector3(x, y, 0);
+    }
+}
