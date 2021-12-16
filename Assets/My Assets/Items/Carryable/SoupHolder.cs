46fa8382c4c6a28adfe014f23bf418d386484728
BluishGreenPro <bluish-green_productions@hotmail.com>
2021-12-15 22:17:06 -0500

    Added SoupHolder script

---
+++ Assets/My Assets/Items/Carryable/SoupHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoupHolder : MonoBehaviour
+{
+    public string soupID;
+    public List<GameObject> soups = new List<GameObject>();
+
+    private Inventory inventory;
+
+    private void OnEnable() {
+        inventory = Inventory.instance;
+        inventory.OnItemChanged += inventory_OnItemChanged;
+    }
+
+    private void OnDisable() {
+        inventory.OnItemChanged -= inventory_OnItemChanged;
+    }
+
+    private void inventory_OnItemChanged(Item item) {
+        int _soups = (int)inventory.InventoryCount(soupID);
+        switch(_soups) {
+            case 0: Destroy(gameObject); break;
+            default:
+                foreach (var soup in soups) { //Turn off all Soup models
+                    soup.SetActive(false);
+                }
+                soups[_soups].SetActive(true); //Except the one that has the correct count
+                break;
+        }
+    }
+}
