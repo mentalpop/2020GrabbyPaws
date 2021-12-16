0607130a527f060f7dc42bcc90d613ffd1d70066
Kaitlyn <43460243+mentalpop@users.noreply.github.com>
2021-12-16 15:16:34 -0500

    Soup Weirdness

--- Assets/My Assets/Items/Carryable/SoupHolder.cs
+++ Assets/My Assets/Items/Carryable/SoupHolder.cs
@@ -15,13 +15,14 @@ public class SoupHolder : MonoBehaviour
     private void Awake()
     {
         PlayersRig = FindObjectOfType<Rig>();
-        PlayersRig.weight = 1.0f;
+        
     }
 
     private void OnEnable()
     {
         inventory = Inventory.instance;
         inventory.OnItemChanged += inventory_OnItemChanged;
+        PlayersRig.weight = 1.0f;
     }
 
     private void OnDisable()
