28b4f9be9e3f7330d04c19b65fefd6539e9f4201
Kaitlyn <43460243+mentalpop@users.noreply.github.com>
2021-12-15 21:34:55 -0500

    Holdables + INT PWUCSUP

---
+++ Assets/ActiveHoldEm.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveHoldEm : MonoBehaviour
+{
+
+    public Transform HoldEmsParent;
+    Holdables holdable;
+    public UnityEngine.Animations.Rigging.Rig handik;
+    public int soupNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Holdables existingHoldable = GetComponentInChildren<Holdables>();
+        if (existingHoldable)
+        {
+            Hold(existingHoldable);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (soupNumber > 1)
+        {
+            handik.weight = 1.0f;
+
+        }
+
+        else
+        {
+            handik.weight = 0.0f;
+        }
+    }
+
+    public void Hold(Holdables newHoldable)
+    {
+        if (holdable)
+        {
+            Destroy(holdable.gameObject);
+            {
+                holdable = newHoldable;
+                holdable.transform.parent = HoldEmsParent;
+                holdable.transform.localPosition = Vector3.zero;
+                holdable.transform.localRotation = Quaternion.identity;
+                handik.weight = 1.0f;
+            }
+        }
+    }
+}
---
+++ Assets/HoldablePickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldablePickup : MonoBehaviour
+{
+
+    public Holdables holdablefab;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        ActiveHoldEm activeHoldEm = other.gameObject.GetComponent<ActiveHoldEm>();
+        if (activeHoldEm)
+        {
+            Holdables newHoldable = Instantiate(holdablefab);
+            activeHoldEm.Hold(newHoldable);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
